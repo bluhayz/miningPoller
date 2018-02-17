f13445b95b9bdde6557bd4339600746491d3c548
bluhayz <bluhayz@drews-mbp.lan>
2018-02-17 09:28:03 -0500

    Updated base as workign to add in functions and clean up code, as well as output to csv.  Should make it easier to keep track of multiple wallets

--- currentBalance.py
+++ currentBalance.py
@@ -1,34 +1,64 @@
 # import libraries
 import urllib2
+import re
 from bs4 import BeautifulSoup
 from time import gmtime, strftime
+from exchanges.bitfinex import Bitfinex
+import csv
 
+btc = Bitfinex().get_current_price()
 time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-# specify the url
-#quote_page = "https://www.ahashpool.com/wallet.php?wallet=1PHCV247CMzpXCiaRzCwRJ2Fn7ggcn4vbM"
-quote_page = "https://www.ahashpool.com/wallet_wallet_results.php?wallet=1PHCV247CMzpXCiaRzCwRJ2Fn7ggcn4vbM"
+#given a wallet this function will return the balance in btc earned and
+#the total uncleared balance earned in btc from ahashpool
+def pullBalanceFromWallet(wallet):
+    # specify the url
+    address = "https://www.ahashpool.com/wallet_wallet_results.php?wallet=" + wallet
+    page = urllib2.urlopen(address)
 
-# query the website and return the html to the variable page
-page = urllib2.urlopen(quote_page)
+    # parse the html using beautiful soup and store in variable `soup`
+    soup = BeautifulSoup(page, 'html.parser')
+    rows = soup.findAll("tr", { "class" : "ssrow" })
 
-# parse the html using beautiful soup and store in variable `soup`
-soup = BeautifulSoup(page, 'html.parser')
+    #grab table with all the values
+    data=[]
+    for row in rows:
+        cells = row.find_all("td")
+        cells = [ele.text.strip() for ele in cells]
+        data.append([ele for ele in cells if ele])
 
-rows = soup.findAll("tr", { "class" : "ssrow" })
+    #figure out the values for balance, unpaid, and paid
+    for keyval in data:
+        if keyval[0] == u'Balance (cleared)':
+            clearedval = keyval[1]
+        if keyval[0] == u'Total Unpaid (pending clearing + cleared)':
+            unclearedval = keyval[1]
+    #Page annoyingly adds BTC to the output so just grabbing the digits
+    balancearr = re.findall(r'\d+\.\d+', clearedval)
+    uncleararr = re.findall(r'\d+\.\d+', unclearedval)
+    return balancearr[0], uncleararr[0]
 
-#print table
-data=[]
-for row in rows:
-    cells = row.find_all("td")
-    cells = [ele.text.strip() for ele in cells]
-    data.append([ele for ele in cells if ele])
-#print data
-balance = 0
-#figure out the values for balance, unpaid, and paid
-for keyval in data:
-    #print keyval[0] + ": " +keyval[1]
-    if keyval[0] == u'Balance':
-        balance = keyval[1]
+wallet = "17QaUWVZ2uGnXbCMiQwHwCs11FMaTSk7a"
+balance, unclearbal = pullBalanceFromWallet(wallet)
 
-print time + ", " + balance
+#Calculate dollar values based off current price at time of poll
+totalearned = float(balance) * float(btc)
+totaluncleared = float(unclearbal) * float(btc)
+
+#print "Total earned: " + str(totalearned)
+
+#create output for the csv file to store the data
+mylist =[]
+mylist.append(wallet)
+mylist.append(time)
+mylist.append(str(balance))
+mylist.append(str(unclearbal))
+mylist.append(totalearned)
+mylist.append(totaluncleared)
+mylist.append(str(btc))
+
+print mylist
+
+with open('currentbalance.csv', 'a') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow(mylist)
